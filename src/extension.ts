db95138d4c934378567462de3027264c5a7c473e
Pine Wu <octref@gmail.com>
2018-02-13 10:39:27 -0800

    fix bad code lol

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,28 @@
-'use strict';
+'use strict'
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
+import * as path from 'path';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	vscode.commands.registerCommand('extension.saveImage', e => {
-		
-	});
-	vscode.commands.registerCommand('extension.sayHello', () => {
-		vscode.commands.executeCommand('vscode.previewHtml',
-			vscode.Uri.file('/Users/penlv/code/vscode-live-clipboard/src/index.html'),
-			2,
-			'Clipboard',
-			{
-				allowScripts: true
-			}
-		);
-	})
+	const htmlPath = path.resolve(context.extensionPath, 'src/index.html');
+
+	vscode.commands.registerCommand('extension.saveImage', e => {})
+
+  vscode.commands.registerCommand('extension.sayHello', () => {
+    vscode.commands.executeCommand(
+      'vscode.previewHtml',
+      vscode.Uri.file(htmlPath),
+      2,
+      'Clipboard',
+      {
+        allowScripts: true
+      }
+    )
+  })
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
