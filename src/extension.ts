6c19773377b88c15fc35a7fd069b831b60fe9060
Pine Wu <octref@gmail.com>
2018-02-15 14:09:28 -0800

    handle flie picker cancel

--- src/extension.ts
+++ src/extension.ts
@@ -20,7 +20,9 @@ export function activate(context: vscode.ExtensionContext) {
         }
       })
       .then(uri => {
-        writeSerializedBlobToFile(serializedBlob, uri.path)
+        if (uri) {
+          writeSerializedBlobToFile(serializedBlob, uri.path)
+        }
       })
   })
 
