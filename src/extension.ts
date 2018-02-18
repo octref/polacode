c33f8c1f11062ba928393013b90c4f3bacc0c4ca
Pine Wu <octref@gmail.com>
2018-02-18 00:32:13 -0800

    remember last used image path. fix #11

--- src/extension.ts
+++ src/extension.ts
@@ -12,30 +12,34 @@ export function activate(context: vscode.ExtensionContext) {
   const htmlPath = path.resolve(context.extensionPath, 'src/webview/index.html')
   const indexUri = vscode.Uri.file(htmlPath)
 
+  let lastUsedImageUri = vscode.Uri.file(path.resolve(homedir(), 'Desktop/code.png'))
   vscode.commands.registerCommand('_polacode.shutter', serializedBlob => {
     vscode.window
       .showSaveDialog({
-        defaultUri: vscode.Uri.file(path.resolve(homedir(), 'Desktop/code.png')),
+        defaultUri: lastUsedImageUri,
         filters: {
-          'Images': ['png']
+          Images: ['png']
         }
       })
       .then(uri => {
         if (uri) {
           writeSerializedBlobToFile(serializedBlob, uri.fsPath)
+          lastUsedImageUri = uri
         }
       })
   })
 
   vscode.commands.registerCommand('polacode.activate', () => {
-    vscode.commands.executeCommand('vscode.previewHtml', indexUri, 2, 'Polacode 📸', {
-      allowScripts: true
-    }).then(() => {
-      const fontFamily = vscode.workspace.getConfiguration('editor').fontFamily
-      vscode.commands.executeCommand('_workbench.htmlPreview.postMessage', indexUri, {
-        type: 'initFontFamily',
-        fontFamily
+    vscode.commands
+      .executeCommand('vscode.previewHtml', indexUri, 2, 'Polacode 📸', {
+        allowScripts: true
       })
-    })
+      .then(() => {
+        const fontFamily = vscode.workspace.getConfiguration('editor').fontFamily
+        vscode.commands.executeCommand('_workbench.htmlPreview.postMessage', indexUri, {
+          type: 'initFontFamily',
+          fontFamily
+        })
+      })
   })
 }
