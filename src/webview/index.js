ab8e2200a5a828f612a3dd7877f04877dfca0193
Pine Wu <octref@gmail.com>
2018-02-19 14:36:09 -0800

    temp fix for html pasted content validation. fix #28. fix #26

--- src/webview/index.js
+++ src/webview/index.js
@@ -96,7 +96,7 @@ function stripInitialIndent(html, indent) {
 
 document.addEventListener('paste', e => {
   const innerHTML = e.clipboardData.getData('text/html')
-  if (!innerHTML.startsWith(`<meta charset='utf-8'>`)) {
+  if (!innerHTML.startsWith(`<meta`)) {
     postMessage('invalidPasteContent')
     return
   }
