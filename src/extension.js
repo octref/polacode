1e7a92e837a6bc820bf686a93d84d0ad580dd52c
Pine Wu <octref@gmail.com>
2019-03-31 11:07:59 -0700

    Restore original functionality in webview API

--- src/extension.js
+++ src/extension.js
@@ -6,77 +6,81 @@ const { homedir } = require('os')
 
 const writeSerializedBlobToFile = (serializeBlob, fileName) => {
   const bytes = new Uint8Array(serializeBlob.split(','))
-  writeFileSync(fileName, new Buffer(bytes))
+  writeFileSync(fileName, Buffer.from(bytes))
 }
 
 const P_TITLE = 'Polacode 📸'
 
 /**
- * @param {vscode.ExtensionContext} context 
+ * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
   const htmlPath = path.resolve(context.extensionPath, 'webview/index.html')
   const indexUri = vscode.Uri.file(htmlPath)
 
   let lastUsedImageUri = vscode.Uri.file(path.resolve(homedir(), 'Desktop/code.png'))
-  vscode.commands.registerCommand('polacode.shoot', serializedBlob => {
-    vscode.window
-      .showSaveDialog({
-        defaultUri: lastUsedImageUri,
-        filters: {
-          Images: ['png']
-        }
-      })
-      .then(uri => {
-        if (uri) {
-          writeSerializedBlobToFile(serializedBlob, uri.fsPath)
-          lastUsedImageUri = uri
-        }
-      })
-  })
+  let panel
 
   vscode.commands.registerCommand('polacode.activate', () => {
-    const panel = vscode.window.createWebviewPanel('polacode', P_TITLE, 2, {
+    panel = vscode.window.createWebviewPanel('polacode', P_TITLE, 2, {
       enableScripts: true,
-      localResourceRoots: [
-        vscode.Uri.file(path.join(context.extensionPath, 'webview'))
-      ]
-    });
-    
-    panel.webview.html = fs.readFileSync(htmlPath, 'utf-8')
+      localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, 'webview'))]
+    })
 
-    // vscode.commands
-    //   .executeCommand('vscode.previewHtml', indexUri, 2, 'Polacode 📸', {
-    //     allowScripts: true,
-    //   })
-    //   .then(() => {
-    //     const fontFamily = vscode.workspace.getConfiguration('editor').fontFamily
-    //     const bgColor = context.globalState.get('polacode.bgColor', '#2e3440')
-    //     vscode.commands.executeCommand('_workbench.htmlPreview.postMessage', indexUri, {
-    //       type: 'init',
-    //       fontFamily,
-    //       bgColor
-    //     })
-    //   })
+    panel.webview.html = getHtmlContent(htmlPath)
+
+    panel.webview.onDidReceiveMessage(({ type, data }) => {
+      switch (type) {
+        case 'shoot':
+          vscode.window
+            .showSaveDialog({
+              defaultUri: lastUsedImageUri,
+              filters: {
+                Images: ['png']
+              }
+            })
+            .then(uri => {
+              if (uri) {
+                writeSerializedBlobToFile(data.serializedBlob, uri.fsPath)
+                lastUsedImageUri = uri
+              }
+            })
+          break
+        case 'updateBgColor':
+          context.globalState.update('polacode.bgColor', data.bgColor)
+          break
+        case 'invalidPasteContent':
+          vscode.window.showInformationMessage(
+            'Pasted content is invalid. Only copy from VS Code and check if your shortcuts for copy/paste have conflicts.'
+          )
+          break
+      }
+    })
+
+    const fontFamily = vscode.workspace.getConfiguration('editor').fontFamily
+    const bgColor = context.globalState.get('polacode.bgColor', '#2e3440')
+    panel.webview.postMessage({
+      type: 'init',
+      fontFamily,
+      bgColor
+    })
   })
 
   vscode.window.onDidChangeTextEditorSelection(e => {
     if (e.selections[0] && !e.selections[0].isEmpty) {
       vscode.commands.executeCommand('editor.action.clipboardCopyAction')
-      vscode.commands.executeCommand('_workbench.htmlPreview.postMessage', indexUri, {
+      panel.postMessage({
         type: 'update'
       })
     }
   })
+}
 
-  vscode.commands.registerCommand('polacode._onmessage', ({ type, data }) => {
-    if (type === 'updateBgColor') {
-      context.globalState.update('polacode.bgColor', data.bgColor)
-    } else if (type === 'invalidPasteContent') {
-      vscode.window.showInformationMessage(
-        'Pasted content is invalid. Only copy from VS Code and check if your shortcuts for copy/paste have conflicts.'
-      )
-    }
+function getHtmlContent(htmlPath) {
+  const htmlContent = fs.readFileSync(htmlPath, 'utf-8')
+  return htmlContent.replace(/script src="([^"]*)"/g, (match, src) => {
+    const realSource = 'vscode-resource:' + path.resolve(htmlPath, '..', src)
+    return `script src="${realSource}"`
   })
 }
 
--- webview/index.js
+++ webview/index.js
@@ -1,177 +1,176 @@
-const snippetNode = document.getElementById('snippet')
-const snippetContainerNode = document.getElementById('snippet-container')
-const obturateur = document.getElementById('save')
+;(function() {
+  const vscode = acquireVsCodeApi()
 
-const getInitialHtml = fontFamily => {
-  const cameraWithFlashEmoji = String.fromCodePoint(128248)
-  const monoFontStack = `${fontFamily},SFMono-Regular,Consolas,DejaVu Sans Mono,Ubuntu Mono,Liberation Mono,Menlo,Courier,monospace`
-  return `<meta charset="utf-8"><div style="color: #d8dee9;background-color: #2e3440; font-family: ${monoFontStack};font-weight: normal;font-size: 12px;line-height: 18px;white-space: pre;"><div><span style="color: #8fbcbb;">console</span><span style="color: #eceff4;">.</span><span style="color: #88c0d0;">log</span><span style="color: #d8dee9;">(</span><span style="color: #eceff4;">'</span><span style="color: #a3be8c;">0. Run command \`Polacode ${cameraWithFlashEmoji}\`</span><span style="color: #eceff4;">'</span><span style="color: #d8dee9;">)</span></div><div><span style="color: #8fbcbb;">console</span><span style="color: #eceff4;">.</span><span style="color: #88c0d0;">log</span><span style="color: #d8dee9;">(</span><span style="color: #eceff4;">'</span><span style="color: #a3be8c;">1. Copy some code</span><span style="color: #eceff4;">'</span><span style="color: #d8dee9;">)</span></div><div><span style="color: #8fbcbb;">console</span><span style="color: #eceff4;">.</span><span style="color: #88c0d0;">log</span><span style="color: #d8dee9;">(</span><span style="color: #eceff4;">'</span><span style="color: #a3be8c;">2. Paste into Polacode view</span><span style="color: #eceff4;">'</span><span style="color: #d8dee9;">)</span></div><div><span style="color: #8fbcbb;">console</span><span style="color: #eceff4;">.</span><span style="color: #88c0d0;">log</span><span style="color: #d8dee9;">(</span><span style="color: #eceff4;">'</span><span style="color: #a3be8c;">3. Click the button ${cameraWithFlashEmoji}</span><span style="color: #eceff4;">'</span><span style="color: #d8dee9;">)</span></div></div></div>`
-}
+  const snippetNode = document.getElementById('snippet')
+  const snippetContainerNode = document.getElementById('snippet-container')
+  const obturateur = document.getElementById('save')
 
-const serializeBlob = (blob, cb) => {
-  const fileReader = new FileReader()
-
-  fileReader.onload = () => {
-    const bytes = new Uint8Array(fileReader.result)
-    cb(Array.from(bytes).join(','))
-  }
-  function getBrightness(color) {
-    const rgb = this.toRgb()
-    return (rgb.r * 299 + rgb.g * 587 + rgb.b * 114) / 1000
+  const getInitialHtml = fontFamily => {
+    const cameraWithFlashEmoji = String.fromCodePoint(128248)
+    const monoFontStack = `${fontFamily},SFMono-Regular,Consolas,DejaVu Sans Mono,Ubuntu Mono,Liberation Mono,Menlo,Courier,monospace`
+    return `<meta charset="utf-8"><div style="color: #d8dee9;background-color: #2e3440; font-family: ${monoFontStack};font-weight: normal;font-size: 12px;line-height: 18px;white-space: pre;"><div><span style="color: #8fbcbb;">console</span><span style="color: #eceff4;">.</span><span style="color: #88c0d0;">log</span><span style="color: #d8dee9;">(</span><span style="color: #eceff4;">'</span><span style="color: #a3be8c;">0. Run command \`Polacode ${cameraWithFlashEmoji}\`</span><span style="color: #eceff4;">'</span><span style="color: #d8dee9;">)</span></div><div><span style="color: #8fbcbb;">console</span><span style="color: #eceff4;">.</span><span style="color: #88c0d0;">log</span><span style="color: #d8dee9;">(</span><span style="color: #eceff4;">'</span><span style="color: #a3be8c;">1. Copy some code</span><span style="color: #eceff4;">'</span><span style="color: #d8dee9;">)</span></div><div><span style="color: #8fbcbb;">console</span><span style="color: #eceff4;">.</span><span style="color: #88c0d0;">log</span><span style="color: #d8dee9;">(</span><span style="color: #eceff4;">'</span><span style="color: #a3be8c;">2. Paste into Polacode view</span><span style="color: #eceff4;">'</span><span style="color: #d8dee9;">)</span></div><div><span style="color: #8fbcbb;">console</span><span style="color: #eceff4;">.</span><span style="color: #88c0d0;">log</span><span style="color: #d8dee9;">(</span><span style="color: #eceff4;">'</span><span style="color: #a3be8c;">3. Click the button ${cameraWithFlashEmoji}</span><span style="color: #eceff4;">'</span><span style="color: #d8dee9;">)</span></div></div></div>`
   }
 
-  fileReader.readAsArrayBuffer(blob)
-}
+  const serializeBlob = (blob, cb) => {
+    const fileReader = new FileReader()
 
-function postMessage(type, data) {
-  window.parent.postMessage(
-    {
-      command: 'did-click-link',
-      data: `command:polacode._onmessage?${encodeURIComponent(JSON.stringify({ type, data }))}`
-    },
-    'file://'
-  )
-}
-function shoot(serializedBlob) {
-  window.parent.postMessage(
-    {
-      command: 'did-click-link',
-      data: `command:polacode.shoot?${encodeURIComponent(JSON.stringify(serializedBlob))}`
-    },
-    'file://'
-  )
-}
+    fileReader.onload = () => {
+      const bytes = new Uint8Array(fileReader.result)
+      cb(Array.from(bytes).join(','))
+    }
+    function getBrightness(color) {
+      const rgb = this.toRgb()
+      return (rgb.r * 299 + rgb.g * 587 + rgb.b * 114) / 1000
+    }
 
-function getBrightness(hexColor) {
-  const rgb = parseInt(hexColor.slice(1), 16)
-  const r = (rgb >> 16) & 0xff
-  const g = (rgb >> 8) & 0xff
-  const b = (rgb >> 0) & 0xff
-  return (r * 299 + g * 587 + b * 114) / 1000
-}
-function isDark(hexColor) {
-  return getBrightness(hexColor) < 128
-}
-function getSnippetBgColor(html) {
-  return html.match(/background-color: (#[a-fA-F0-9]+)/)[1]
-}
-
-function updateEnvironment(snippetBgColor) {
-  // update snippet bg color
-  document.getElementById('snippet').style.backgroundColor = snippetBgColor
-
-  // update backdrop color
-  if (isDark(snippetBgColor)) {
-    snippetContainerNode.style.backgroundColor = '#f2f2f2'
-  } else {
-    snippetContainerNode.style.background = 'none'
+    fileReader.readAsArrayBuffer(blob)
   }
-}
 
-function getMinIndent(code) {
-  const arr = code.split('\n')
+  function shoot(serializedBlob) {
+    vscode.postMessage({
+      type: 'shoot',
+      data: {
+        serializedBlob
+      }
+    })
+  }
 
-  let minIndentCount = Number.MAX_VALUE
-  for (let i = 0; i < arr.length; i++) {
-    const wsCount = arr[i].search(/\S/)
-    if (wsCount !== -1) {
-      if (wsCount < minIndentCount) {
-        minIndentCount = wsCount
+  function getBrightness(hexColor) {
+    const rgb = parseInt(hexColor.slice(1), 16)
+    const r = (rgb >> 16) & 0xff
+    const g = (rgb >> 8) & 0xff
+    const b = (rgb >> 0) & 0xff
+    return (r * 299 + g * 587 + b * 114) / 1000
+  }
+  function isDark(hexColor) {
+    return getBrightness(hexColor) < 128
+  }
+  function getSnippetBgColor(html) {
+    return html.match(/background-color: (#[a-fA-F0-9]+)/)[1]
+  }
+
+  function updateEnvironment(snippetBgColor) {
+    // update snippet bg color
+    document.getElementById('snippet').style.backgroundColor = snippetBgColor
+
+    // update backdrop color
+    if (isDark(snippetBgColor)) {
+      snippetContainerNode.style.backgroundColor = '#f2f2f2'
+    } else {
+      snippetContainerNode.style.background = 'none'
+    }
+  }
+
+  function getMinIndent(code) {
+    const arr = code.split('\n')
+
+    let minIndentCount = Number.MAX_VALUE
+    for (let i = 0; i < arr.length; i++) {
+      const wsCount = arr[i].search(/\S/)
+      if (wsCount !== -1) {
+        if (wsCount < minIndentCount) {
+          minIndentCount = wsCount
+        }
       }
     }
+
+    return minIndentCount
   }
 
-  return minIndentCount
-}
-
-function stripInitialIndent(html, indent) {
-  const doc = new DOMParser().parseFromString(html, 'text/html')
-  const initialSpans = doc.querySelectorAll('div > div span:first-child')
-  for (let i = 0; i < initialSpans.length; i++) {
-    initialSpans[i].textContent = initialSpans[i].textContent.slice(indent)
-  }
-  return doc.body.innerHTML
-}
-
-document.addEventListener('paste', e => {
-  const innerHTML = e.clipboardData.getData('text/html')
-
-  const code = e.clipboardData.getData('text/plain')
-  const minIndent = getMinIndent(code)
-
-  const snippetBgColor = getSnippetBgColor(innerHTML)
-  postMessage('updateBgColor', { bgColor: snippetBgColor })
-  updateEnvironment(snippetBgColor)
-
-  if (minIndent !== 0) {
-    snippetNode.innerHTML = stripInitialIndent(innerHTML, minIndent)
-  } else {
-    snippetNode.innerHTML = innerHTML
-  }
-})
-
-obturateur.addEventListener('click', () => {
-  const width = snippetContainerNode.offsetWidth * 2
-  const height = snippetContainerNode.offsetHeight * 2
-  const config = {
-    width,
-    height,
-    style: {
-      transform: 'scale(2)',
-      'transform-origin': 'left top'
+  function stripInitialIndent(html, indent) {
+    const doc = new DOMParser().parseFromString(html, 'text/html')
+    const initialSpans = doc.querySelectorAll('div > div span:first-child')
+    for (let i = 0; i < initialSpans.length; i++) {
+      initialSpans[i].textContent = initialSpans[i].textContent.slice(indent)
     }
+    return doc.body.innerHTML
   }
 
-  domtoimage.toBlob(snippetContainerNode, config).then(blob => {
-    serializeBlob(blob, serializedBlob => {
-      shoot(serializedBlob)
+  document.addEventListener('paste', e => {
+    const innerHTML = e.clipboardData.getData('text/html')
+
+    const code = e.clipboardData.getData('text/plain')
+    const minIndent = getMinIndent(code)
+
+    const snippetBgColor = getSnippetBgColor(innerHTML)
+    vscode.postMessage({
+      type: 'updateBgColor',
+      data: {
+        bgColor: snippetBgColor 
+      }
+    })
+    updateEnvironment(snippetBgColor)
+
+    if (minIndent !== 0) {
+      snippetNode.innerHTML = stripInitialIndent(innerHTML, minIndent)
+    } else {
+      snippetNode.innerHTML = innerHTML
+    }
+  })
+
+  obturateur.addEventListener('click', () => {
+    const width = snippetContainerNode.offsetWidth * 2
+    const height = snippetContainerNode.offsetHeight * 2
+    const config = {
+      width,
+      height,
+      style: {
+        transform: 'scale(2)',
+        'transform-origin': 'left top'
+      }
+    }
+
+    domtoimage.toBlob(snippetContainerNode, config).then(blob => {
+      serializeBlob(blob, serializedBlob => {
+        shoot(serializedBlob)
+      })
     })
   })
-})
 
-let isInAnimation = false
+  let isInAnimation = false
 
-obturateur.addEventListener('mouseover', () => {
-  if (!isInAnimation) {
-    isInAnimation = true
+  obturateur.addEventListener('mouseover', () => {
+    if (!isInAnimation) {
+      isInAnimation = true
 
-    new Vivus(
-      'save',
-      {
-        duration: 40,
-        onReady: () => {
-          obturateur.className = 'obturateur filling'
+      new Vivus(
+        'save',
+        {
+          duration: 40,
+          onReady: () => {
+            obturateur.className = 'obturateur filling'
+          }
+        },
+        () => {
+          setTimeout(() => {
+            isInAnimation = false
+            obturateur.className = 'obturateur'
+          }, 700)
         }
-      },
-      () => {
-        setTimeout(() => {
-          isInAnimation = false
-          obturateur.className = 'obturateur'
-        }, 700)
-      }
-    )
-  }
-})
-
-window.addEventListener('message', e => {
-  if (e) {
-    if (e.data.type === 'init') {
-      const { fontFamily, bgColor } = e.data
-
-      const initialHtml = getInitialHtml(fontFamily)
-      snippetNode.innerHTML = initialHtml
-
-      // update backdrop color, using bgColor from last pasted snippet
-      // cannot deduce from initialHtml since it's always using Nord color
-      if (isDark(bgColor)) {
-        snippetContainerNode.style.backgroundColor = '#f2f2f2'
-      } else {
-        snippetContainerNode.style.background = 'none'
-      }
-
-      snippetContainerNode.style.opacity = '1'
-    } else if (e.data.type === 'update') {
-      document.execCommand('paste')
+      )
     }
-  }
-})
+  })
+
+  window.addEventListener('message', e => {
+    if (e) {
+      if (e.data.type === 'init') {
+        const { fontFamily, bgColor } = e.data
+
+        const initialHtml = getInitialHtml(fontFamily)
+        snippetNode.innerHTML = initialHtml
+
+        // update backdrop color, using bgColor from last pasted snippet
+        // cannot deduce from initialHtml since it's always using Nord color
+        if (isDark(bgColor)) {
+          snippetContainerNode.style.backgroundColor = '#f2f2f2'
+        } else {
+          snippetContainerNode.style.background = 'none'
+        }
+
+        snippetContainerNode.style.opacity = '1'
+      } else if (e.data.type === 'update') {
+        document.execCommand('paste')
+      }
+    }
+  })
+})()
